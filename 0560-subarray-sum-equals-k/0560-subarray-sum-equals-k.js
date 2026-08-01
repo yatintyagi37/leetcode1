d2c4db17ffc12687dfc0f50276a74bad50385f20
Yatin Tyagi <yatintyagi37@gmail.com>
2026-08-01 11:31:23 +0530

    Time: 17 ms (68.79%), Space: 64.9 MB (28.75%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    const map = new Map();
+    map.set(0, 1);
+;
+
+    let sum = 0;
+    let count = 0;
+
+    for (let num of nums){
+        sum += num;
+
+        if (map.has(sum - k)){
+            count += map.get(sum - k);
+        }
+
+        map.set(sum, (map.get(sum) || 0) + 1);
+    }
+
+    return count;
+};
