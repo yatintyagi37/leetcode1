1f81870f6a90b8a35519d50008b5166bcdc4956c
Yatin Tyagi <yatintyagi37@gmail.com>
2026-08-02 07:19:48 +0530

    Time: 0 ms (100%), Space: 29.5 MB (6.87%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int HammingWeight(int n) {
+        int count = 0;
+
+        while(n != 0){
+            if ((n & 1) == 1){
+                count++;
+            }
+            n >>>= 1;
+        }
+
+        return count;
+        
+    }
+}
