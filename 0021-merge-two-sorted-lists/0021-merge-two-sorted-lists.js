4d3c9e82aa5f5a8a6ddec623ba0ea9cd6476e374
Yatin Tyagi <yatintyagi37@gmail.com>
2026-08-03 18:10:10 +0530

    Time: 1 ms (36.57%), Space: 56.7 MB (77.27%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,26 @@
+var mergeTwoLists = function(list1, list2) {
+    
+    let dummy = new ListNode(0);
+    let current = dummy;
+
+    while (list1 !== null && list2 !== null){
+
+        if (list1.val < list2.val){
+            current.next = list1;
+            list1 = list1.next;
+        }
+        else{
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+
+    if (list1 !== null){
+        current.next = list1;
+    }
+    else{
+        current.next = list2;
+    }
+    return dummy.next;
+};
