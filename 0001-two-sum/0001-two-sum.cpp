d0aa47f954a347f8beaffa2e54d096ecc10bf5a5
Yatin Tyagi <yatintyagi37@gmail.com>
2026-08-05 11:18:25 +0530

    Time: 0 ms (100%), Space: 14.9 MB (40.5%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp; // value -> index
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {}; // No solution found
+    }
+};
